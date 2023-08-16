b0b2f7fb1d65bd13a99941b64736dbe3ee93ac7a
sofiasastre <sofisaspa@gmail.com>
2023-08-16 17:20:36 -0300

    clase04vivo

---
+++ Modulo2/C4S-TrabajandoConComponentes/clase04vivo/ejemplomapykey/src/App.js
@@ -0,0 +1,39 @@
+import "./styles.css";
+
+const disciplinas = [
+  "Banco de Dados",
+  "Frontend",
+  "Backend",
+  "Metodologias Ágeis",
+  "Infraestrutura"
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Disciplinas</h1>
+
+      {/*
+          Primeiro mostrar sem a utilização do Map.
+      */}
+
+      <ul>
+        <li>Banco de Dados</li>
+        <li>Frontend</li>
+        <li>Backend</li>
+        <li>Metodologias Ágeis</li>
+        <li>Infraestrutura</li>
+      </ul>
+
+      {/*
+          Depois mostrar com a utilização do Map.
+      */}
+
+      <ul>
+        {disciplinas.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
