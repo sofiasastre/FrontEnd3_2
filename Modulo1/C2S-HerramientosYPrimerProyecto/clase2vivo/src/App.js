85397296778785ee80010a22e5f832a8191e4eee
sofiasastre <sofisaspa@gmail.com>
2023-08-15 23:11:38 -0300

    Clase01V03

---
+++ Modulo1/C2S-HerramientosYPrimerProyecto/clase2vivo/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Bienvenidos a Front 3</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
