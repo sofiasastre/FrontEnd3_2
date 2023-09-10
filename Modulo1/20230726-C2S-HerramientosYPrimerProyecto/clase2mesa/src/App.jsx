770bbd9cef734c90e3a7b987922922b1c9c6a287
sofiasastre <sofisaspa@gmail.com>
2023-09-10 03:26:44 -0300

    2023091003:26 C2

---
+++ Modulo1/20230726-C2S-HerramientosYPrimerProyecto/clase2mesa/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+      <header className='App-header'>
+      <h1> Viva React!!!!! </h1>
+      <img src="https://i.redd.it/legdlji9yl771.jpg" alt="memeReact"></img>
+   
+     </header>
+    </div>
+    </>
+  )
+}
+
+export default App
